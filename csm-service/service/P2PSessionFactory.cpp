5d7d1df2445ee77ba591e094eb90cc16a52f29d5
chuwen <786675283@qq.com>
2026-04-14 10:36:52 +0800

    1. 去除RingBuffer的init()方法调用

--- csm-service/service/P2PSessionFactory.cpp
+++ csm-service/service/P2PSessionFactory.cpp
@@ -12,9 +12,7 @@ using namespace csm::service;
 P2PSession::Ptr P2PSessionFactory::create(const int fd, const std::size_t readBufferSize, const std::size_t writeBufferSize)
 {
     std::unique_ptr<utilities::RingBuffer> readBuffer = std::make_unique<utilities::RingBuffer>(readBufferSize);
-    readBuffer->init();
     std::unique_ptr<utilities::RingBuffer> writeBuffer = std::make_unique<utilities::RingBuffer>(writeBufferSize);
-    writeBuffer->init();
 
     P2PSession::Ptr p2pSession = std::make_shared<P2PSession>(fd, m_sessionId, std::move(readBuffer), std::move(writeBuffer));
     ++m_sessionId;
